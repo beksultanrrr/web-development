02ed3b41b46c210a77b9824ff00693777bf25b46
Elvina <elvinashm@gmail.com>
2021-02-24 12:56:40 +0600

    lab4 online store started

---
+++ Lab4/lab4-angular/src/app/products.ts
@@ -0,0 +1,113 @@
+export const products = [
+
+  {
+    id: 1,
+    name: 'Laptop Stand Adjustable Laptop Computer Stand Multi-Angle Stand Phone Stand Portable Foldable Laptop Riser Notebook Holder',
+    price: 19.88,
+    description: 'Adjustable - Eight heights can be freely adjusted from 5.5inch to 7.0inch. You can straighten your waist and relieve cervical and neck fatigue, feel comfortable to work',
+    url: 'https://www.amazon.com/MeFee-Adjustable-Computer-Multi-Angle-Compatible/dp/B07T4G7RJY/ref=sr_1_9?dchild=1&qid=1614141982&s=computers-intl-ship&sr=1-9',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/71l8JpvNk4L._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/61B7ZXoXORL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/71kcAgNEWKL._AC_SL1500_.jpg',
+    rating: '5.png'
+  },
+  {
+    id: 2,
+    name: 'Acer SB220Q bi 21.5 Inches Full HD (1920 x 1080) IPS Ultra-Thin Zero Frame Monitor (HDMI & VGA Port), Black',
+    price: 93.99,
+    description: '21.5 inches Full HD (1920 x 1080) widescreen IPS display. And Radeon free sync technology. No compatibility for VESA Mount. Refresh rate: 75 hertz - Using HDMI port',
+    url: 'https://www.amazon.com/Acer-SB220Q-Ultra-Thin-Frame-Monitor/dp/B07CVL2D2S/ref=lp_16225007011_1_5',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/71xb7GDuAGL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/51tuEFDaPhL._AC_SL1500_.jpg',
+    rating: '5.png'
+  },
+  {
+    id: 3,
+    name: 'Sennheiser HD280PRO Headphone (new model)',
+    price: 98.99,
+    description: 'Dynamic, closed ear headphones.Weight w/o cable: 285 gram Ear coupling is circumaural',
+    url: 'https://www.amazon.com/Sennheiser-HD280PRO-Headphone-new-model/dp/B00IT0IHOY/ref=sr_1_30?dchild=1&qid=1614142246&s=electronics&sr=1-30',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/615Nba6dy4L._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/81p7i7MMcaL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/51pSVnUSGoL._AC_SL1500_.jpg',
+    rating: '4.png'
+  },
+  {
+    id: 4,
+    name: 'Logitech Keyboard Folio',
+    price: 17.87,
+    description: 'This Certified Refurbished product is tested & certified by Logitech to work like-new. The product includes all original accessories, and is backed by a 90 day warranty.',
+    url: 'https://www.amazon.com/Logitech-Keyboard-Folio-iPad-Refurbished/dp/B00VP4GYL4/ref=sr_1_41?dchild=1&keywords=apple&qid=1614142421&s=specialty-aps&sr=1-41',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81gGdKBFlDL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/51PvYRksGYL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/81-F8p22JeL._AC_SL1500_.jpg',
+    rating: '4.png'
+  },
+  {
+    id: 5,
+    name: 'Apple MacBook Pro with Intel Processor (13-inch, 16GB RAM, 512GB SSD Storage) - Silver',
+    price: 1679.32,
+    description: '1.4GHz quad-core 8th-generation Intel Core i5 with Turbo Boost up to 3.9GHz or 2.0GHz quad-core 10th-generation Intel Core i5 with Turbo Boost up to 3.8GHz	',
+    url: 'https://www.amazon.com/Apple-MacBook-13-inch-Storage-Keyboard/dp/B0882JG168/ref=sr_1_10?crid=1HD61PRBTW1US&dchild=1&keywords=apple%2Bmacbook&qid=1614142456&s=specialty-aps&sprefix=apple%2B%2Cdeals-intl-ship%2C415&sr=1-10-catcorr&th=1',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/71ppucPX3wL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/71OE0Ic7ScL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/81Wjw%2B0Gu4L._AC_SL1500_.jpg',
+    rating: '5.png'
+  },
+  {
+    id: 6,
+    name: 'Tissyee 96W USB C Power Adapter, Compatible with MacBook Pro Air Charger',
+    price: 38.99,
+    description: 'Fast charging 96W Usb C power adapter: highly efficient reach up to 92% electrical energy conversion rate, which can charge your notebook quickly without compromising its performance and longevity',
+    url: 'https://www.amazon.com/Tissyee-Adapter-Compatible-MacBook-Charger/dp/B08K8MFTRN/ref=sr_1_8?crid=1HD61PRBTW1US&dchild=1&keywords=apple+macbook&qid=1614142456&s=specialty-aps&sprefix=apple+%2Cdeals-intl-ship%2C415&sr=1-8-catcorr',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/619OCoAAAmL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/71OCVeJPsKL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/81EKaMfseKL._AC_SL1500_.jpg',
+    rating: '5.png'
+  },
+  {
+    id: 7,
+    name: 'Logitech Gaming Mouse',
+    price: 48.50,
+    description: 'Logitech G502 Hero High Performance Gaming Mouse. Hero 25K sensor through a software update from G HUB, this upgrade is free to all players: Our most advanced, with 1:1 tracking, 400+ ips, and 100 - 25,600 max dpi sensitivity plus zero smoothing, filtering, or acceleration.',
+    url: 'https://www.amazon.com/Logitech-G502-Performance-Gaming-Mouse/dp/B07GBZ4Q68/ref=sr_1_4?dchild=1&fst=as%3Aoff&pf_rd_i=16225007011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=74069509-93ef-4a3c-8dca-a9e3fa773a64&pf_rd_r=WAAV16XD56HQAJWQ7G77&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614123518&rnid=172456&s=computers-intl-ship&sr=1-4',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/61RWbWU5xaL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/71OX1L%2BrvbL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/71hcd1bEjLL._AC_SL1500_.jpg',
+    rating: '3.png'
+  },
+  {
+    id: 8,
+    name: 'Curved Monitor',
+    price: 311,
+    description: '27" Super Curved Frameless Gaming Monitor QHD 2K, 1500R Curved VA, 1ms. AOC RE-SPAWNED 3-year zero-bright-dot 3-year advance replacement 1-year (one-time) accidental damage coverage.',
+    url: 'https://www.amazon.com/AOC-CQ27G2-Frameless-Adjustable-Guarantee/dp/B0862YHJ56/?_encoding=UTF8&pd_rd_w=OhlK9&pf_rd_p=520d89b4-8b70-4df1-a4e9-4164b6c84c16&pf_rd_r=EHBY0YKJGTK7BKJFW17C&pd_rd_r=1c63ab3b-dd19-4b69-a423-3716a8eb2b81&pd_rd_wg=eMGW1&ref_=pd_gw_trq_rep_sims_gw',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81oORaahKZL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/71tKAIpgr3L._AC_SL1500_.jpg',
+    image3: '',
+    rating: '3.png'
+  },
+  {
+    id: 9,
+    name: 'Apple iPad Pro',
+    price: 899,
+    description: '12.9-inch edge-to-edge Liquid Retina display with ProMotion, True Tone, and P3 wide color. A12Z Bionic chip with Neural Engine. Face ID for secure authentication and Apple Pay.',
+    url: 'https://www.amazon.com/Apple-iPad-12-9-inch-Wi-Fi-256GB/dp/B0862HNWGK/ref=pd_rhf_gw_s_pd_crcd_0_3?_encoding=UTF8&ie=UTF8&pd_rd_i=B0862HNWGK&pd_rd_r=BE5XEPE6Z99NNA9PZGH5&pd_rd_w=gKdlt&pd_rd_wg=d8siy&pf_rd_p=8019ba47-0a12-4976-b76b-5c932d60db6f&pf_rd_r=BE5XEPE6Z99NNA9PZGH5&pf_rd_s=recent-history-footer&pf_rd_t=gateway&psc=1&refRID=BE5XEPE6Z99NNA9PZGH5',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81SGb5l%2BlZL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/91zyHoys2jL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/71oxyKvOTbL._AC_SL1500_.jpg',
+    rating: '4.png'
+  },
+  {
+    id: 10,
+    name: 'HUANUO Lap Laptop Desk - Portable Lap Desk with Pillow Cushion',
+    price: 39.99,
+    description: '✓HIGHLY VERSATILE DESIGN - This computer desk comes with an anti-slip strip and storage pocket, which provides you with extra space and protection',
+    url: 'https://www.amazon.com/HUANUO-Lap-Laptop-Desk-Anti-Slip/dp/B07W7SZJ25/ref=sr_1_17?dchild=1&qid=1614141982&s=computers-intl-ship&sr=1-17',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81zIq7Ez3KL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/81OO5cNDOLL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/71CxTpNkATL._AC_SL1500_.jpg',
+    rating: '4.png'
+  },
+];
